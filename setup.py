58495c9fbd3d04d5c7d4f9b5896c852e28e84a86
Nathan Spencer <natekspencer@gmail.com>
2021-02-11 09:56:01 -0700

    Update build/setup files

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import setup
+import setuptools
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setup(
-    name="pylitterbot",
-    version="2021.2.3",
-    description="Python package for controlling a Litter-Robot Connect self-cleaning litter box.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Nathan Spencer",
-    author_email="natekspencer@gmail.com",
-    url="https://github.com/natekspencer/pylitterbot",
-    license="Licensed under the MIT license. See LICENSE file for details",
-    packages=["pylitterbot"],
-    package_dir={"pylitterbot": "pylitterbot"},
-    install_requires=["Authlib", "httpx"],
-)
+setuptools.setup()
