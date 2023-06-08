fa12c95069ae1e75602af498d0bd54ce7580ae89
Nathan Spencer <natekspencer@gmail.com>
2023-06-08 12:57:47 -0600

    Poetry package and deploy updates

--- pylitterbot/__init__.py
+++ pylitterbot/__init__.py
@@ -1,5 +1,5 @@
 """pylitterbot module."""
-__version__ = "2023.4.0"
+__version__ = "0.0.0"
 
 from .account import Account
 from .robot import Robot
--- tests/test_init.py
+++ tests/test_init.py
@@ -4,4 +4,4 @@ from pylitterbot import __version__
 
 def test_version() -> None:
     """Test the version."""
-    assert __version__ == "2023.4.0"
+    assert __version__ == "0.0.0"
